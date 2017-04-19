68d99c808f29d1d4d01d1bc262471d38f7c02c48
Taylor Mott <trm36@humboldt.edu>
2017-04-19 13:00:44 -0600

    Initial Commit - Start Point

---
+++ BrickBreaker/GameScene.swift
@@ -0,0 +1,38 @@
+//
+//  GameScene.swift
+//  BrickBreaker
+//
+//  Created by Taylor Mott on 19.4.17.
+//  Copyright © 2017 Mott Applications. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
